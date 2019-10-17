b0503a5f6e6aeb9b4b936401cb796d7047043226
Eduardo Ludi <eduludi@gmail.com>
2019-10-18 01:18:09 +0200

    Updates App and index

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom'
+
+import Welcome from './screens/Welcome'
+import Login from './screens/Login'
+import SignUp from './screens/SignUp'
+import ForgotPassword from './screens/ForgotPassword'
+import Donate from './screens/Donate'
+import DonationsRouter from './screens/Donations/Router'
+import Profile from './screens/Profile'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container font-sans ">
+      <Router>
+        <Switch>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <SignUp />
+          </Route>
+          <Route path="/forgotPassword">
+            <ForgotPassword />
+          </Route>
+          <Route path="/donate">
+            <Donate />
+          </Route>
+          <Route path="/donations">
+            <DonationsRouter />
+          </Route>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+          <Redirect from="/" to="/welcome" />
+        </Switch>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
