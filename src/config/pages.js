ae9e12c6dcf991271e899ac2e5287b30a50b9774
Eduardo Ludi <eduludi@gmail.com>
2019-10-18 01:17:20 +0200

    Adds innerr pages configuration

---
+++ src/config/pages.js
@@ -0,0 +1,29 @@
+export const innerPages = [
+  {
+    name: 'donate',
+    label: 'Donate',
+    url: '/donate',
+    icon: 'donate',
+    inMenu: true,
+  },
+  {
+    name: 'donations',
+    label: 'Donations',
+    url: '/donations',
+    icon: 'list-alt',
+    inMenu: true,
+  },
+  {
+    name: 'donationsDetails',
+    label: 'Donation details',
+    url: '/donations/details',
+    icon: 'list-alt',
+  },
+  {
+    name: 'profile',
+    label: 'Profile',
+    url: '/profile',
+    icon: 'user-circle',
+    inMenu: true,
+  },
+]
