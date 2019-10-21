653a4c5e5819ec21710a91f65aa8ca3c093db9fb
Néstor Escobar <nestorescobar@gmail.com>
2019-10-21 16:08:17 +0200

    Improves screens/Donations

--- src/screens/Donations/Donations.js
+++ src/screens/Donations/Donations.js
@@ -12,18 +12,18 @@ function rand(number) {
 
 const projects = [
   'Internet',
-  'Thite',
+  'Tithe',
   'Offerings',
   'ADRA',
-  'Youth Ministery',
-  'Child Ministery',
+  "Youth's Ministries",
+  "Child's Ministries",
   'Project X',
   'Project Y',
   'Project Z',
 ]
 
 const donations = range(0, 100).map(i => ({
-  date: `${rand(31)}.${rand(12)}.2019`,
+  date: `${rand(31)}/${rand(12)}/2019`,
   type: projects[rand(4)],
   ammount: `${rand(250)}.${rand(99)}`,
 }))
