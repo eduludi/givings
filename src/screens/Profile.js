4be261f23e342681156d3aaca84da14754d8f491
Néstor Escobar <nestorescobar@gmail.com>
2019-10-21 16:06:24 +0200

    Adds improvements to screens/Profile

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -3,6 +3,9 @@ import React from 'react'
 import Screen from '../ui/Screen'
 import Header from '../ui/Header'
 import Field from '../ui/Field'
+import Section from '../ui/Section'
+import Item from '../ui/Item'
+import { range } from '../utils/array'
 
 const countries = [
   {
@@ -39,18 +42,25 @@ const countries = [
   },
 ]
 
+function rand(number) {
+  return Math.floor(Math.random() * number) + 1
+}
+
+const donations = range(0, 10).map(i => ({
+  date: `${rand(31)}/${rand(12)}/2019`,
+  ammount: `${rand(250)}.${rand(99)}`,
+}))
+
 function Profile() {
   return (
     <Screen title="My profile">
-      <Header
-        title="Sascha Schuster"
-        subtitle="sascha.schuster@stimme-der-hoffnung.de"
-      />
+      <Header title="Sascha Schuster" subtitle="Darmstadt International" />
       <Field
         label="E-mail"
         type="email"
         initialValue="sascha.schuster@stimme-der-hoffnung.de"
       />
+      <Field label="Street and number" initialValue="Sandwiesenstraße 35" />
       <Field label="Postal Code" initialValue="64665" />
       <Field label="City" initialValue="Alsbach-Hähnlein" />
       <Field
@@ -59,6 +69,16 @@ function Profile() {
         initialValue="de"
         options={countries}
       />
+      <Section title="Last donations" more="See all" to="/donations">
+        {donations.map(({ date, ammount }, key) => (
+          <Item
+            key={`donation-${key}`}
+            label={date}
+            value={`${ammount}€`}
+            to="/donations/details"
+          />
+        ))}
+      </Section>
     </Screen>
   )
 }
