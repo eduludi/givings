03b514cf904deb1d6a537c74b1ae4998e05aa540
Néstor Escobar <nestorescobar@gmail.com>
2019-10-21 16:07:58 +0200

    Improves text to screens/Donations/Details

--- src/screens/Donations/Details.js
+++ src/screens/Donations/Details.js
@@ -7,22 +7,22 @@ import Screen from '../../ui/Screen'
 
 function DonnationDetails() {
   return (
-    <Screen title="Donation details">
-      <Header title="Donation #34569" subtitle="on 10.10.2019" />
+    <Screen title="Donation detail">
+      <Header title="Donation #34569" subtitle="on 10/10/2019" />
 
       <Section>
-        <Item label="Total" value="1221.67 €" />
+        <Item label="Total" value="593.34 €" />
       </Section>
 
       <Section title="To local church">
-        <Item label="18.09.2019" sublabel="Thite" value="221.67 €" />
-        <Item label="17.09.2019" sublabel="Offerings" value="50.00 €" />
+        <Item label="18/09/2019" sublabel="Tithe" value="221.67 €" />
+        <Item label="17/09/2019" sublabel="Offerings" value="50.00 €" />
       </Section>
 
       <Section title="To Union Projects">
-        <Item label="12.09.2019" sublabel="Offerings" value="50.00 €" />
-        <Item label="15.08.2019" sublabel="Thite" value="221.67 €" />
-        <Item label="12.08.2019" sublabel="Offerings" value="50.00 €" />
+        <Item label="12/09/2019" sublabel="Online church" value="50.00 €" />
+        <Item label="15/08/2019" sublabel="Caleb" value="221.67 €" />
+        <Item label="12/08/2019" sublabel="His Hands" value="50.00 €" />
       </Section>
     </Screen>
   )
