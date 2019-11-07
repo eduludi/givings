60e08e48a85948d02cbfa3adafa462db42c4a7e2
Néstor Escobar <nestorescobar@gmail.com>
2019-11-07 10:57:12 +0100

    Adds a new build

---
+++ build/precache-manifest.b427371c4ad45f2028f623cf43f8b984.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0abce7e21037c39c2e26bd7ecb68046",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10b92bcd28fd90bec006",
+    "url": "/static/js/2.eb28a77b.chunk.js"
+  },
+  {
+    "revision": "f6d6e662b79ec8e5c9d4",
+    "url": "/static/js/main.c3b7f09f.chunk.js"
+  },
+  {
+    "revision": "df27b005bd36188a8c30",
+    "url": "/static/js/runtime-main.3219cde8.js"
+  }
+]);
